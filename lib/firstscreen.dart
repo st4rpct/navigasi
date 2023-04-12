3c8759b93ff3d30d83594e07028623b7239fd78e
Palcomtech <admin@palcomtech.ac.id>
2023-04-12 11:21:16 +0700

    simple routing

---
+++ lib/firstscreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi/secondscreen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("Layar Pertama"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondScreen())
+              );
+            },
+            child: Text("Menuju Layar Kedua")),
+      ),
+    );
+  }
+}
---
+++ lib/secondscreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text("Layar Kedua"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("Kembali")),
+      ),
+    );
+  }
+}
